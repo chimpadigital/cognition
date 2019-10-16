3169ae342ee70a13ebdfc4117fbf07d7a56723c0
chimpadigital <sdesigncba@gmail.com>
2019-10-16 14:51:03 -0300

    ajuste form

--- send_mail.php
+++ send_mail.php
@@ -3,7 +3,7 @@
 This first bit sets the email address that you want the form to be submitted to.
 You will need to change this value to a valid email address that you can access.
 */
-$webmaster_email = "sanjeev@purplecode.biz";
+$webmaster_email = "sprados@chimpancedigital.com.ar";
 
 /*
 This bit sets the URLs of the supporting pages.
@@ -17,12 +17,13 @@ $thankyou_page = "thank_you.html";
 This next bit loads the form field data into variables.
 If you add a form field, you will need to add it here.
 */
-$email_address = $_REQUEST['email_address'] ;
+$email = $_REQUEST['email_address'] ;
 $comments = $_REQUEST['comments'] ;
-$first_name = $_REQUEST['first_name'] ;
+$name = $_REQUEST['first_name'] ;
 $msg = 
-"First Name: " . $first_name . "\n" . 
-"Email: " . $email_address . "\n" . 
+"Nombre: " . $name . "\n" . 
+"Email: " . $email . "\n" . 
+"Teléfono: " . $phone . "\n" . 
 "Comments: " . $comments ;
 
 /*
@@ -69,7 +70,7 @@ header( "Location: $error_page" );
 // If we passed all previous tests, send the email then redirect to the thank you page.
 else {
 
-	mail( "$webmaster_email", "Feedback Form Results", $msg );
+	mail( "$webmaster_email", "Consulta web Cognition", $msg );
 
 	header( "Location: $thankyou_page" );
 }
