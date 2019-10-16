e782b5db04171d72b60abe90139c8efa44852302
chimpadigital <sdesigncba@gmail.com>
2019-10-16 16:04:24 -0300

    cambio remitente

--- contact_me.php
+++ contact_me.php
@@ -72,7 +72,7 @@ $mail->setFrom('info@cognition.com', 'Cognition');
 
 $mail->addReplyTo('sprados@chimpancedigital.com.ar','Cognition');
 
-$mail->addAddress('sprados@chimpancedigital.com.ar','Cognition');
+$mail->addAddress('sdesigncba@gmail.com','Cognition');
 // $mail->addCc('cristiancastro.pr1991@gmail.com','Clínica Santia Lucia');
 // $mail->addCc('quirofanosantaluciasalta@gmail.com','Clínica Santia Lucia');
 $mail->isHTML(true);
